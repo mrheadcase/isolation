5b70919892cb9d7d50778402793cd444f686a979
Justin <justinhead@gmail.com>
2025-08-09 19:32:55 -0600

    Update to Node.js 20 and clean vite config to fix crypto.hash CI error

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react()
-  ],
+  plugins: [react()],
   base: '/isolation/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    sourcemap: false
+    sourcemap: false,
+    target: 'es2015'
   },
   server: {
     port: 3000,
